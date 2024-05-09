e8bc2cd61465f55ecb8392e3d520118d3ccd9a8f
bigGoing123 <1256405274@qq.com>
2024-05-09 17:22:09 +0800

    公共字段自动填充

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -20,6 +22,7 @@ public interface EmployeeMapper {
 
     @Insert("insert into employee(username, password, name, phone, sex, id_number,  status, create_time, update_time)" +
             "values(#{username}, #{password}, #{name}, #{phone}, #{sex}, #{idNumber},#{status}, #{createTime}, #{updateTime})")
+    @AutoFill(value = OperationType.INSERT)
     void save(Employee employee);
 
 
@@ -34,6 +37,7 @@ public interface EmployeeMapper {
      * 更新员工状态
      * @param employee
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
 
